0aeeef554250e00616b97a3274146ec405e37860
Jonathan Soltren <91296386+KapitanJon@users.noreply.github.com>
2023-01-30 21:03:50 -0500

    more php

--- signup.php
+++ signup.php
@@ -3,7 +3,7 @@ $showAlert = false;
 $shoeError = false;
 $exists = false;
 
-if($_Server["REQUEST_METHOD"] == "POST"){
+if($_SERVER["REQUEST_METHOD"] == "POST"){
 
     include 'checkconn.php';
 
@@ -21,7 +21,55 @@ if($_Server["REQUEST_METHOD"] == "POST"){
 
     $num = mysqli_num_rows($result);
     
-    #if($)
+    if($num == 0){
+        if($exists == false){
+            $hash = password_hash($password, PASSWORD_DEFAULT);
+
+            $sql = "INSERT INTO `entries` (`userName`, `userEmail`, `Password`, `User FirstName`) VALUES ('$username', '$useremail', '$hash','$userfirstname')";
+
+            $result = mysqli_query($conn, $sql);
+
+                if ($result){
+                    $showAlert = true;
+
+                }
+            }
+    
+        if($num > 0){
+        $exists = "Username or Email already in use";
+            }
 
 
-}
+        }
+    }
+?>
+
+<!doctype html>
+
+<head>
+        <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div id = "header">
+            <h1>Sign Up</h1>
+            <div id = "Button">
+                <a href="login.html">Login</a>
+                <a href="landing.html">Home</a>
+            </div>
+        </div>
+    <?php
+
+        if($showAlert){
+            echo ' <div class = "green-alert" role="alert">
+            
+            <strong>Account Created</strong>
+            
+            </div>';
+        }
+        ?>
+
+        <h2>Information</h2>
+
+
+    </body>
+    </html>
