a8894fb003b6e75947cf2f24a9e4ae824ad63491
Will1o7 <wevans4@broncos.uncfsu.edu>
2023-04-05 10:20:49 -0400

    Pushing changes: using modal boxes for challenges

---
+++ JSFiles/Scripts.js
@@ -0,0 +1,8 @@
+function clickEvent(){var answer = prompt("What is 2+2?");
+            if (answer == 4 || answer =="four"){
+                txt = "Correct!";
+            } else {
+                txt = "Incorrect, Try Again"
+            }
+            alert(txt);
+}
