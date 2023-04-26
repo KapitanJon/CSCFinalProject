e9f37b17bfdbc0ae65b306aea1d745f6ee1ec52f
ford-from-the-future <khalic44@gmail.com>
2023-04-26 10:33:42 -0400

    ion even know

--- test.php
+++ test.php
@@ -1,3 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" href="test.css">
+    </head>
+    <body>
+        <div id = "header">
+            <h1>Practice    </h1>
+            <div id = "Button">
+                <a href="dashboard.php">Dashboard</a>
+                <a href="logout.php">Logout</a>
+            </div>
+        </div>
+        <h2>Choose what you want to practice</h2>
+        <a href="basicmath.php">Arithmetic</a>
+        <a href="basicprogramming.php">Basic Programming</a>
+        <a href="clitests.php">CLI Knowledge</a>
+
+        
+    </body>
+</html>
 <?php session_start();?>
 <!DOCTYPE html>
 <html>
@@ -16,5 +37,10 @@
         <a href="basicmath.php">Arithmetic</a>
         <a href="basicprogramming.php">Basic Programming</a>
         <a href="clitests.php">CLI Knowledge</a>
+        <h2><?php if(isset($_SESSION['message'])){
+                echo $_SESSION['message'];
+                unset($_SESSION['message']);
+            }?>
+        </h2>
     </body>
 </html>
