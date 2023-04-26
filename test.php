c22891d4aef7318a4cb1263d94e35a8e134d1046
Jonathan Soltren <91296386+KapitanJon@users.noreply.github.com>
2023-04-26 12:14:30 -0400

    Fixed test Page

--- test.php
+++ test.php
@@ -1,23 +1,3 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <link rel="stylesheet" href="test.css">
-    </head>
-    <body>
-        <div id = "header">
-            <h1>Practice    </h1>
-            <div id = "Button">
-                <a href="dashboard.php">Dashboard</a>
-                <a href="logout.php">Logout</a>
-            </div>
-        </div>
-        <h2>Choose what you want to practice</h2>
-        <a href="basicmath.php">Arithmetic</a>
-        <a href="basicprogramming.php">Basic Programming</a>
-        <a href="clitests.php">CLI Knowledge</a>
-
-        
-    </body>
 </html>
 <?php session_start();?>
 <!DOCTYPE html>
