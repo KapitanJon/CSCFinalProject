7bd3577bbc3f1cc96c940d28ace6903ef09e6d3a
ford-from-the-future <khalic44@gmail.com>
2023-05-02 11:48:59 -0400

    update menu on dashboard

--- test.php
+++ test.php
@@ -16,6 +16,8 @@
         <a href="basicmath.php">Arithmetic</a>
         <a href="basicprogramming.php">Basic Programming</a>
         <a href="clitests.php">CLI Knowledge</a>
+        <a href="dashboard.php">Dashboard</a>
+        <a href="logout.php">Logout</a>
         <h2><?php if(isset($_SESSION['message'])){
                 echo $_SESSION['message'];
                 unset($_SESSION['message']);
