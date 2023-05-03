64da7fb26a10689e877f8397db90cb70fd3b6291
ford-from-the-future <khalic44@gmail.com>
2023-05-02 20:53:44 -0400

    updated titles

--- basicprogramming.php
+++ basicprogramming.php
@@ -1,14 +1,14 @@
 <html>
     <head>
         <title>Basic Programming</title>
-        <link rel="stylesheet" href="tracks.css"> 
+        <link rel="stylesheet" href="basic_program_style.css"> 
     </head>
     <body>
         <header>
             <h1>Basic Programming</h1>
+            <h2>Test Basic Programming Skills</h2>
         </header>
         <main>
-            <p>Test Basic Programming Skills</p>
             <a href="basicprogrammingquestions.php?n=1" class="start">Let's Go</a>
         </main>
     </body>
