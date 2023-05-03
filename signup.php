9c86ef66d4e668093b3293db7e633c8b9268d5ac
Jonathan Soltren <91296386+KapitanJon@users.noreply.github.com>
2023-05-03 13:47:25 -0400

    Some changes

--- signup.php
+++ signup.php
@@ -7,12 +7,21 @@ $link = mysqli_connect("127.0.0.1", "root", "", "users");
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
- 
+
+
 // Escape user inputs for security
 $name = mysqli_real_escape_string($link, $_REQUEST['name']);
 $email = mysqli_real_escape_string($link, $_REQUEST['email']);
 $password = mysqli_real_escape_string($link, $_REQUEST['password']);
 $hashed_password = password_hash($password,PASSWORD_DEFAULT);
+$sql=mysqli_query($link,"SELECT * FROM users WHERE email = '$email'");
+if(mysqli_num_rows($sql)>0){
+    $_SESSION['message'] = "Email in use!";
+    header("Location:signup.html");
+    echo "Email in use";
+    exit;
+
+} 
 if(!isset($name,$email,$password) || trim($name)=='' or trim($email)=='' or trim($password)==''){
     echo '<script>alert("Fill all Fields")</script>';
     header("Location: signup.html");
