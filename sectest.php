8b2f9692aefbda48cc07309220bae0055dbd02e6
Jonathan Soltren <91296386+KapitanJon@users.noreply.github.com>
2023-05-03 11:54:16 -0400

    security tests

---
+++ sectest.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>Security Tests</title>
+        <link rel="stylesheet" href="basicmath_style.css">
+    </head>
+    <body>
+        <header>
+            <h1>Cyber Security</h1>
+            <h2>Test CyberSecurity</h2>
+        </header>
+        <main>
+            <a href="basicmathquestions.php?n=1" class="start">Let's Go</a>
+            <a href="test.php?n=1" class="start">Return to Tests</a>
+        </main>
+    </body>
+
+    <div class="footer">
+    <p>CSC 490 Senior Project for Fayetteville State University</p>
+  </div>
