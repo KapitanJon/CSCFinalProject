e64287c2451958d3a4e755102dd3b34f50bc6a93
ford-from-the-future <khalic44@gmail.com>
2023-05-02 20:28:24 -0400

    addedd footer to basicmath file

--- basicmath.php
+++ basicmath.php
@@ -12,3 +12,7 @@
             <a href="basicmathquestions.php?n=1" class="start">Let's Go</a>
         </main>
     </body>
+
+    <div class="footer">
+    <p>CSC 490 Senior Project for Fayetteville State University</p>
+  </div>
