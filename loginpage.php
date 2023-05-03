cdb99537853e7640788eb36304287fd1a82498f5
ford-from-the-future <khalic44@gmail.com>
2023-05-03 09:29:00 -0400

    making container

--- loginpage.php
+++ loginpage.php
@@ -16,23 +16,23 @@
             </div>
             <span onclick="openNav()" class="button">Menu</span>
         </div>
-
-        <div class="container">
-            <form id="loginform" action="login.php" method="post" >
-                <p>Login</p>
-                <p>
-                <label for="Email">Email:</label>
-                <input type="text" name="email" id="email" required="" onfocus="this.value=''" >
-                </p>
-                <p>
-                <label for="password">Password:</label>
-                <input type="password" name="password" id="password" required="" onfocus="this.value=''">
-                </p>
-                <div class="holder">
-                <input type="submit" value="Login">
-                </div>
-        </div>
-
+        <main>
+            <div class="container">
+                <form id="loginform" action="login.php" method="post" >
+                    <p>Login</p>
+                    <p>
+                    <label for="Email">Email:</label>
+                    <input type="text" name="email" id="email" required="" onfocus="this.value=''" >
+                    </p>
+                    <p>
+                    <label for="password">Password:</label>
+                    <input type="password" name="password" id="password" required="" onfocus="this.value=''">
+                    </p>
+                    <div class="holder">
+                    <input type="submit" value="Login">
+                    </div>
+            </div>
+        </main>
     </body>
 </html>
 <?php
